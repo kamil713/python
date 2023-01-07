a1f7a7f7f0340dfac40a1d0f2e0e80bc73ab0fe2
Kamil Pietrzyk <kam.pietrz@gmail.com>
2023-01-07 14:07:53 +0100

    add html lib for convert all named and numeric character references (&#039)

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import connect_to_api
 import nanoid
+import html
 import json
-
-d = dict()
-
 
 def setting_answers(answers, correct_answer):
     answers_list = []
@@ -17,12 +15,13 @@ def setting_answers(answers, correct_answer):
 
 newData = []
 for item in connect_to_api.data['results']:
+    d = dict()
     d['id'] = nanoid.generate(size=10)
     d['category'] = item['category']
-    d['question'] = item['question']
-    all_answers = item['incorrect_answers'] + [item['correct_answer']]
+    d['question'] = html.unescape(item['question'])
+    all_answers = html.unescape(item['incorrect_answers']) + [html.unescape(item['correct_answer'])]
     all_answers.sort()
-    d['answers'] = setting_answers(all_answers, item['correct_answer'])
+    d['answers'] = setting_answers(all_answers, html.unescape(item['correct_answer']))
     newData.append(d)
 
 # print(type(newData))
